172d9cb393b38ff8223e13d01f306aa2f3d99721
Natansh Verma <natansh.verma@gmail.com>
2013-02-28 20:14:30 +0530

    Added page transitions using jquery, menu transitions using css and related improvements/modifications.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$('.home #heading, .home .nav, #container').hide();
 
+$('document').ready(function() {
+  $('#container').fadeIn(1000).queue(function() {
+    $('.home #heading, .home .nav').fadeIn(1500);
+  });
+});
