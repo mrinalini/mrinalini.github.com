4506f53b50fec91cc1d96a2035a9ce9c40a3c2bc
Natansh Verma <natansh.verma@gmail.com>
2013-03-01 14:36:37 +0530

    Working around a stupid Twitter bug in which the username is not shown in case the element has the display property set to none.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ $('.home #heading, .home .nav, #container, #copyright-footer').hide();
 $(window).on('load', function() {
   $('#loader-container').fadeOut(200).queue(function() {
     $('#container').fadeIn(1000).queue(function() {
+
+      /* Need a friggin workaround for the twitter button as it doesn't show @username when display: none is set. */
+      $('#contact-twitter .contact-box').empty().html('<a href="https://twitter.com/AThingOfJoy" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="true">Follow @AThingOfJoy</a>\
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>')
       $('.home #heading, .home .nav, #copyright-footer').fadeIn(1500);
     });
   });
